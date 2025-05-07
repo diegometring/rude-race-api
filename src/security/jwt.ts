b82858e092148a85f3e3c3ab0af519b4cad7fa46
diegometring <diegometringteixeira@gmail.com>
2025-05-07 15:16:22 -0300

    .

--- src/security/jwt.ts
+++ src/security/jwt.ts
@@ -8,8 +8,8 @@ if (!JWT_SECRET) {
     throw new Error('JWT_SECRET não está definido no .env');
 }
 
-export const generateToken = (payload: object, expiresIn: string | number = '1h'): string => {
-    const options: SignOptions = {expiresIn};
+export const generateToken = (payload: object, tokenExpiresIn: string | number = '1h'): string => {
+    const options: SignOptions = {expiresIn: tokenExpiresIn};
     return jwt.sign(payload, JWT_SECRET, options);
 };
 
