bda73cbe7b24821f3050b5b7079ad71b9b8d61d0
diegometring <diegometringteixeira@gmail.com>
2025-05-07 15:53:28 -0300

    .

--- src/security/jwt.ts
+++ src/security/jwt.ts
@@ -1,22 +1,23 @@
-import jwt, {SignOptions} from 'jsonwebtoken';
+import jwt, { SignOptions, JwtPayload } from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET as string;
 if (!JWT_SECRET) {
-    throw new Error('JWT_SECRET não está definido no .env');
+    throw new Error("JWT_SECRET não está definido no .env");
 }
 
 export const generateToken = (payload: object, tokenExpiresIn: string | number = '1h'): string => {
-    const options: SignOptions = {expiresIn: tokenExpiresIn};
+    const options: SignOptions = { expiresIn: tokenExpiresIn };
     return jwt.sign(payload, JWT_SECRET, options);
 };
 
-export const verifyToken = (token: string): any | null => {
+export const verifyToken = (token: string): JwtPayload | null => {
     try {
-        return jwt.verify(token, JWT_SECRET);
+        return jwt.verify(token, JWT_SECRET) as JwtPayload;
     } catch (error) {
+        console.error("Erro na verificação");
         return null;
     }
 };
